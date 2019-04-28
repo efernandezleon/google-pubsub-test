07b7716134918fa47c1d5a54bb0aa47f54d58896
Eduardo Fernández León <eduardo.fernandez@geographica.com>
2019-04-28 18:57:04 +0200

    Improve the code structure

--- app.py
+++ app.py
@@ -18,11 +18,8 @@ async def websocket_handler(request):
     print('Websocket connection ready')
 
     async for msg in ws:
-        if msg.type == WSMsgType.TEXT:
-            if msg.data == 'close':
-                await ws.close()
-            else:
-                await ws.send_str(msg.data)
+        if msg.type == WSMsgType.TEXT and msg.data == 'close':
+            await ws.close()
 
     print('Websocket connection closed')
     return ws
@@ -37,37 +34,45 @@ async def send_msg(websockets, message):
         print('Sending ' + message)
         await ws.send_json(message)
 
+def subscribe_to_pubsub(app):
+    # Subscribe to the Pub/Sub topic
+    subscriber = pubsub_v1.SubscriberClient()
+    topic_name = 'projects/{project_id}/topics/{topic}'.format(
+        project_id=project_id,
+        topic=topic
+    )
+    subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
+        project_id=project_id,
+        sub=subscription
+    )
 
-# Subscribe to the Pub/Sub topic
-subscriber = pubsub_v1.SubscriberClient()
-topic_name = 'projects/{project_id}/topics/{topic}'.format(
-    project_id=project_id,
-    topic=topic
-)
-subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
-    project_id=project_id,
-    sub=subscription
-)
+    # Define the callback for the pubsub
+    def callback(message):
+        strmessage = message.data.decode('utf-8')
+        app['data']['last_message'] = strmessage
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        result = loop.run_until_complete(send_msg(app['websockets'], strmessage))
+        message.ack()
 
-# Define the callback for the pubsub
-def callback(message):
-    strmessage = message.data.decode('utf-8')
-    app['data']['last_message'] = strmessage
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    result = loop.run_until_complete(send_msg(app['websockets'], strmessage))
-    message.ack()
+    future = subscriber.subscribe(subscription_name, callback)
 
-future = subscriber.subscribe(subscription_name, callback)
+def create_app():
+    # Start the websocket server
+    app = web.Application()
+    app.add_routes(routes)
+    app.router.add_static('/', path=str('./www/'))
+    app['websockets'] = set()
+    # this is only for testing purposes, for a production mode, the last message should be stored
+    # in a distributed cache like Redis
+    app['data'] = {}
+    app['data']['last_message'] = last_message
+    return app
 
-# Start the websocket server
-app = web.Application()
-app.add_routes(routes)
-app.router.add_static('/', path=str('./www/'))
-app['websockets'] = set()
-# this is only for testing purposes, for a production mode, the last message should be stored
-# in a distributed cache like Redis
-app['data'] = {}
-app['data']['last_message'] = last_message
-web.run_app(app)
+def main():
+    app = create_app()
+    subscribe_to_pubsub(app)
+    web.run_app(app)
 
+if __name__ == "__main__":
+    main()
